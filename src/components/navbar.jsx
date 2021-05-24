3a597224649079605bba5859ecfe9d67d32a675f
Mike Peterson <mpeterson2004@gmail.com>
2021-05-24 13:21:56 -0600

    finished basic construction of navbar class

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+function navbar(){
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">All Songs</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Add New Song</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">Filter Results</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        )
+}
+
+export default navbar
